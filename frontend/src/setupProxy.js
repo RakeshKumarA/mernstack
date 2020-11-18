640b16afe2252090704c4c3ae3a848164be686fa
RakeshKumarA <rkakumar85@gmail.com>
2020-11-18 21:32:04 +0100

    added api

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    '/',
+    '/api',
     createProxyMiddleware({
       target: 'https://backend-twlrpkz6xa-ey.a.run.app',
       changeOrigin: true,
